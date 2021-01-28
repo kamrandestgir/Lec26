6e30999ad583faf3cdb74d14a574391b0c29ca5a
Kamran GujJar <kamrandestgir@gmail.com>
2021-01-28 18:34:30 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lec26/FragmentTwo.java
@@ -0,0 +1,24 @@
+package com.example.lec26;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentTwo extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =inflater.inflate(R.layout.fragment_two, container, false);
+        // paste your code here.
+        return v;
+    }
+}
